b13053b431571c664e66ef2c62341cd125045002
HosseinFree <seyed.fazeli@mail.utoronto.ca>
2016-06-06 20:33:45 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -2,38 +2,51 @@ var time_remained; // to keep track of times
 var is_paused; // to check if the timer is puased or not
 var score;  //to keep the current score
 var level;  // to keep the current level
-var alert_window;   // pause_alert_window
+var alert_window = document.getElementById("alert_wind");   // pause_alert_window
+var timer_control;
 
 function start(){
-  time_remained = 60; 
+  time_remained = 10; 
   score = 200;
   is_paused = 0;
-  level 1;
+  level = 1;
+  document.getElementById("level").innerHTML = level;
+  document.getElementById("score").innerHTML = score;
+  
+  document.getElementById("time_new_game").addEventListener("click",function(){
+                                                                		document.getElementById("time_alert").style.visibility = "hidden";
+																		clearTimeout(timer_control);
+																		start();
+		                                                            });
+  
+  document.getElementById("pause").addEventListener("click",function(){
+                                                                is_paused = 1 ;
+        														document.getElementById("pause_alert").style.visibility = "visible";
+		                                                        });
+  																
+  document.getElementById("resume").addEventListener("click",function(){
+                                                                is_paused = 0 ;
+        														document.getElementById("pause_alert").style.visibility = "hidden";
+		                                                        });																
   show_time();
-  alert_window = document.getElementById("alert_wind");
 }
 
 function show_time(){
-  if( time_remainded == 0 ) {
-    show_timeover_alert();  
+  if( time_remained < 0 && is_paused == 0) {
+    show_timeover_alert();  	
   }else{
-    time_remainded = time_remainded-1; 
-	document.getElementById("time").innerHTML="";
-	document.getElementById("time").innerHTML= time_remainder;
-	setTimeout(show_time,1000);
+    if( is_paused == 0 ){
+	   document.getElementById("time").innerHTML=  time_remained;	  
+	   time_remained = time_remained - 1; 
+	}  
   }
+   timer_control = setTimeout(show_time,1000);
 }
 
 function show_timeover_alert(){
-    var alert_wind = document.createElement("section");
-    alert_wind.style.width = "100px";
-    alert_wind.style.height = "300px";
-    alert_wind.style.marginLeft = "auto";
-    alert_wind.style.marginRight = "auto";
-    alert_wind.style.marginRight = "auto";
-  
+     document.getElementById("time_alert").style.visibility="visible";
+	 
 }
 
-
-window.onload = start;
+window.onload = setTimeout(start,1000);
 
